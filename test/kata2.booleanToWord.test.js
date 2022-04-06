7dad889a541eced869a6c8bf0e657b77307e4980
Maisie James <maisienjames@gmail.com>
2022-04-06 17:03:18 +0100

    booleanToWord

--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,8 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  it("returns 'Yes' when passed True, 'No' when passed False", () => {
+  expect(booleanToWord(true)).toBe('Yes')
+  expect(booleanToWord(false)).toBe('No')
+  })
 });
